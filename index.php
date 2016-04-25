4a23deeeabaec35fe6932bac1942cfd944c5603a
Dom McKenzie <dlm24@njit.edu>
2016-04-24 23:06:27 -0400

    f

---
+++ index.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dominic LMcKenzie</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/stylish-portfolio.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>
+    <nav id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
+            <li class="sidebar-brand">
+                <a href="#top"  onclick = $("#menu-close").click(); ></a>
+            </li>
+            <li>
+                <a href="#top" onclick = $("#menu-close").click(); >Home</a>
+            </li>
+            <li>
+                <a href="#about" onclick = $("#menu-close").click(); >About</a>
+            </li>
+            <li>
+                <a href="#services" onclick = $("#menu-close").click(); >Services</a>
+            </li>
+            <li>
+                <a href="#portfolio" onclick = $("#menu-close").click(); >Portfolio</a>
+            </li>
+            <li>
+                <a href="#contact" onclick = $("#menu-close").click(); >Contact</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- Header -->
+    <header id="top" class="header">
+		
+        <div class="text-vertical-center">
+            <h1><div class="linkedin">
+			<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+			<script type="IN/MemberProfile" data-id="https://www.linkedin.com/in/mckenziedominic" data-format="inline"></script>
+		</div></h1>
+           <!-- <h3>Network Administrator - Web Developer</h3> 
+            <br>
+            <a href="#about" class="btn btn-dark btn-lg">Find Out More</a> --> 
+        </div>
+    </header>
+
+    <!-- About -->
+    <section id="about" class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Objective</h2>
+                    <p class="lead">I am a quality driven Network Administrator and Developer with over 6 years of experience. I am seeking a Network Analyst position that will allow me to contribute to the growth and innovation of the company as I develop and utilize my technical, programming, project, and problem solving skills.</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <!-- Services -->
+    <!-- The circle icons use Font Awesome's stacked icon classes. For more information, visit http://fontawesome.io/examples/ -->
+    <section id="services" class="services bg-primary">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-lg-10 col-lg-offset-1">
+                    <h2>Education</h2>
+                    <hr class="small">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6">
+                            <div class="service-item">
+                                <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-cloud fa-stack-1x text-primary"></i>
+                            </span>
+                                <h4>
+                                    <strong>New Jersey Institute of Technology</strong>
+                                </h4>
+                                <p>In progress of Bachelor of Science (B.S.): Information Technology: Web Applications; May 2017, expected, GPA 3.94</p>
+                                <a href="#" class="btn btn-light">Learn More</a>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="service-item">
+                                <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-compass fa-stack-1x text-primary"></i>
+                            </span>
+                                <h4>
+                                    <strong>Middlesex County College</strong>
+                                </h4>
+                                <p>Associate in Applied Science (A.A.S.): Computer Science: Network Administration, May 2013, GPA 3.57 Certificate of Achievement: Windows/ PC Support, May 2013</p>
+                                <a href="#" class="btn btn-light">Learn More</a>
+                            </div>
+                        </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.col-lg-10 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <!-- Callout -->
+   <!-- <aside class="callout">
+        <div class="text-vertical-center">
+            <h1></h1>
+        </div>
+    </aside> -->
+	 <!-- About -->
+    <section id="about" class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>DATAMATICS MANAGEMENT SERVICES INC., Fords, NJ</h2>
+                    <p class="lead">Systems Implementation Specialist, 05/2013-Present</p>
+					<p> Answers, evaluates, and prioritizes incoming telephone calls, voicemails, emails, and in-person requests for assistance from users experiencing problems with hardware, software, networking, and any other computer related technologies. <br>
+						 Interviews client user to collect information about a problem and leads user through diagnostic procedures to determine source of error. <br>
+						 Handles problem recognition, research, isolation, resolution, and follow-up for routine user problems, escalating more complex problems to the correct technical staff.<br> 
+						 Management of backup systems/ procedures and disaster recovery plans.<br>
+						 Configures and tests bio-metric and time clock hardware. <br>
+						 Network Management, including the installation, configuration, troubleshooting, maintenance, and support of the Internal Network Infrastructure. <br>
+						 Handles shipping, receiving, and tracking of hardware and or IT related inventory via UPS/ FedEx.<br>
+						 Works with software development team in creating, testing, implementing, and maintaining various custom applications and programs. <br>
+						 Logging and tracking of calls and emails using Microsoft Dynamics CRM. <br>
+						 Calls hardware vendors to request service regarding defective products.<br>
+						 Develops training support materials such as documents, exercises, and visual displays. <br>
+						 Trains end users/ administrators on software and or hardware on-site or via the web. <br>
+						 Writes or revises software/ hardware training documents and procedures for management review. <br>
+						 Develop small programs in C#.  Run queries in Microsoft SQL 2012. <br>
+						 Analyzes and evaluates incident reports and makes written recommendations to reduce help line incident rate. <br>
+						 Consults with technical staff to explain software errors or to recommend changes to programs. <br>
+						 Asset management of IT/Network infrastructure inventory and hardware.<br></p>
+                </div>
+            </div>
+			<div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>QDX PATH ALLIANCE, Cranford, NJ</h2>
+                    <p class="lead">IT Support Specialist, 04/2011 to 11/2011 </p>
+					<p> Direct technical support for medium sized company with 125+ employees.<br>
+						 Managed, updated, and backed up Inventory via software.  <br>
+						 Proficient in laboratory software: LabDaQ, LIS.  <br>
+						 Responsible for software updates, upgrades, backups, and maintenance of servers and network. <br>
+						 Created and updated existing IT related policies and SOPs.  <br>
+						 Supervised and trained data entry personnel and data entry procedures. <br>
+						 Assisted as part of a team in creating HL7 interfaces for new machines and EMR systems. <br>
+						 Established remote connections to external client computers using Remote Desktop & LOGMEIN for troubleshooting, updating, support and maintenance.<br> 
+						 Provided professional help desk technical support for numerous clients.<br>
+						 Responsible for shipping, tracking, and receiving via FedEx and UPS.</p>
+                </div>
+            </div>
+			<div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>FLEXTRONICS, Woodbridge, NJ</h2>
+                    <p class="lead">Device Specialist, Customer Support Technician, 02/2008 to 03/2011 </p>
+					<p> Face to face programming, troubleshooting, repair, support, updating, and maintenance of IPhones, Androids, Blackberries, Windows Mobile devices, PDAs, phones, and tablets. <br>
+						 Diffused customer escalations to ensure efficient and effective customer service.  <br>
+						 Responsible for opening and closing procedure of the technical service department. <br>
+						 Selected to assist in the training of 13 new employees; recognized for world class experience to customers; earning 3 service awards. <br>
+						 Responsible for answering technical support calls via telephone<br></p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+	
+    <!-- Call to Action -->
+    <aside class="call-to-action bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h3>Education Links</h3>
+                    <a href="www.njit.edu"  target="_Blank" class="btn btn-lg btn-dark">New Jersey Institute of Technology</a>
+                    <a href="http://www.middlesexcc.edu/" target="_Blank" class="btn btn-lg btn-dark">Middlesex County College</a>
+                </div>
+            </div>
+        </div>
+    </aside>
+<section id="about" class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Development Technical Skills</h2>
+                    <p class="lead">JavaScript - jQuery - node.js - mySQL - PHP - SQL - C#.Net - HTML5 - CSS - JAVA - Visual Basic</p>
+                </div>
+            </div>  
+            <!-- /.row -->
+			<div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Networking Technical Skills</h2>
+                    <p class="lead">TCP/IP - LAN - WLAN - VPN - DNS - DHCP - DMZ - VLAN - WAN - FTP - SSH - RDP<br> Virtual Servers/Desktops; Hyper V, Virtualbox;</p>
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
+    </section>
+	
+    <!-- Portfolio -->
+    <section id="portfolio" class="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                    <h2>Certifications</h2>
+                    <hr class="small">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-portfolio img-responsive" src="img/netplus.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-portfolio img-responsive" src="img/mtablack.png">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-portfolio img-responsive" src="img/mcp.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <!-- <div class="col-md-6">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-portfolio img-responsive" src="img/portfolio-4.jpg">
+                                </a>
+                            </div>
+                        </div> -->
+                    </div>
+                    <!-- /.row (nested) 
+                    <a href="#" class="btn btn-dark">View More Items</a> -->
+                </div>
+                <!-- /.col-lg-10 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <!-- Map -->
+    <!-- <section id="contact" class="map">
+         <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/place/New+Jersey+Institute+of+Technology/@40.7431473,-74.1783086,15z/data=!4m2!3m1!1s0x0:0xb97c287a2ef95f43"></iframe>
+        <br />
+        <small>
+            <a href="https://www.google.com/maps/place/New+Jersey+Institute+of+Technology/@40.7431473,-74.1783086,15z/data=!4m2!3m1!1s0x0:0xb97c287a2ef95f43"></a>
+        </small>
+        </iframe>
+    </section> -->
+
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                    <h4><strong>Dominic L. McKenzie</strong>
+                    </h4>
+                    <p>150 Bleeker St, Newark, NJ 07102</p>
+                    <ul class="list-unstyled">
+                        <li><i class="fa fa-envelope-o fa-fw"></i>  <a href="mailto:name@example.com">dlm24@njit.edu</a><br>
+                        </li>
+                    </ul>
+                    <br> <img class="img-portfolio img-responsive" src="img/njit_logo.gif"><br>
+                    <ul class="list-inline">
+                     <!--   <li>
+                            <a href="#"><i class="fa fa-facebook fa-fw fa-3x"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-dribbble fa-fw fa-3x"></i></a>
+                        </li> -->
+                    </ul>
+                    <hr class="small">
+                    <p class="text-muted">Copyright &copy; is219s16mckenzie-p1.herokuapp.com 2016</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script>
+    // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    </script>
+
+</body>
+
+</html>
